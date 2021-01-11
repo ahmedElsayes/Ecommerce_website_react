17bbbdf67c7396635f6791228c90c55ebecc77a7
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-11 11:35:30 +0200

    Building product details page_part1

--- my-app/src/screens/ProductScreen.js
+++ my-app/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
@@ -8,6 +8,30 @@ export default function ProductScreen({ match }) {
 
     const singleProduct = products.find((p) => {return p._id === match.params.id})
     return (
-        <div> {singleProduct.name} </div>
+        <div>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={singleProduct.image} alt={singleProduct.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{singleProduct.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating
+                                value={singleProduct.rating}
+                                text={`${singleProduct.numReviews} reviews`}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${singleProduct.price}</ListGroup.Item>
+                        <ListGroup.Item>Description: {singleProduct.description}</ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </div>
     )
 }
