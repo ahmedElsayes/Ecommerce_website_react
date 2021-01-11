609b9efdb00613116ae8cf1b9988b0b5a435010a
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-11 10:26:31 +0200

    Matching product with URI request

--- my-app/src/screens/ProductScreen.js
+++ my-app/src/screens/ProductScreen.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-export default function ProductScreen() {
+export default function ProductScreen({ match }) {
+
+    const singleProduct = products.find((p) => {return p._id === match.params.id})
     return (
-        <div> Product </div>
+        <div> {singleProduct.name} </div>
     )
 }
