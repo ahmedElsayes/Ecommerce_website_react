1414883d167a9d97dcd9fa3f039c66c529a79bd8
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-13 08:32:52 +0200

    2-connection to DataBase with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req,res) => {
@@ -20,4 +25,4 @@ app.get('/api/products/:id', (req,res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode port ${PORT}`))
+app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold))
