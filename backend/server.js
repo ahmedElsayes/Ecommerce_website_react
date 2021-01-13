ea3c0c6fd73b0d51669ad89c656b98bb59e65e7f
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-13 07:12:30 +0200

    7-Environment configuration and ES6 syntax in NodeJS

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req,res) => {
@@ -16,4 +18,6 @@ app.get('/api/products/:id', (req,res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log("server is running on port 5000"))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode port ${PORT}`))
