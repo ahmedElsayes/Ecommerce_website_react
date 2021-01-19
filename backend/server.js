54d6ac1a56f82dc36611351665dd5727b1ac0deb
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-19 21:07:51 +0200

    Generic error handling

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,18 @@ app.get('/', (req,res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json(err.message)
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold))
