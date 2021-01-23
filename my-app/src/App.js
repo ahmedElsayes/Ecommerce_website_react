34d719e7f999bfcc66665e3bf88eebb5cfd78c3f
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-23 22:11:02 +0200

    Enabling multiple units from single product

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen'
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={ProductScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer/>
--- my-app/src/screens/ProductScreen.js
+++ my-app/src/screens/ProductScreen.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 //import products from '../products'
 // import axios from 'axios'
 
-export default function ProductScreen({ match }) {
+export default function ProductScreen({ history, match }) {
+
+    const [qty, setQty] = useState(1)
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
 
     // const [product, setProduct] = useState([])
 
@@ -73,8 +79,29 @@ export default function ProductScreen({ match }) {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col>
+                                            <Form.Control
+                                                as="select"
+                                                value={qty}
+                                                onChange={(e) => setQty(e.target.value)}
+                                            >
+                                                {[...Array(product.countInStock).keys()].map((x) => (
+                                                    <option key={x+1} value={x+1}>
+                                                        {x+1}
+                                                    </option>
+                                                ))}
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
                             <ListGroup.Item>
                                 <Button
+                                onClick= {addToCartHandler}
                                 className='btn-block'
                                 type='button'
                                 disabled={product.countInStock === 0}
