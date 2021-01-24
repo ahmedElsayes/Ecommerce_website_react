e66180170d64fabea61c101b98e6d8a65f2063e5
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-24 20:24:54 +0200

    Using controllers in the back-end

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 
-router.get('/', asyncHandler(async (req,res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
-router.get('/:id', asyncHandler(async (req,res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404).json({message: 'product not found'})
-    }
-    
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
