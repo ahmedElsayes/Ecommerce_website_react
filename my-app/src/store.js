26740c21dfe8525454d2e6ff599b398da51da99c
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-30 19:17:33 +0200

    Redux store and reducer for user

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { productListReducer, productDetailsReducer } from './reducers/productReducer'
+import { userLoginReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
@@ -8,14 +9,20 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer
   })
   
   const cartItemsFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
   
+  const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : []
+
   const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
   }
   
   const middleware = [thunk]
