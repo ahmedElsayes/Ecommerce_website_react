d89106357614fe8aad844c9026acf1aa67851c83
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-30 13:34:08 +0200

    New user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,38 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// for creating new users
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+
+})
+
 const getUserProfile = asyncHandler(async (req, res) => {
     // res.send('successful calling')
     const user = await User.findById(req.user._id)
@@ -37,4 +69,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
