9fd928f371cd29ff7aed3b0e27da1a50a2b78790
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-01-31 12:19:00 +0200

    4-Enable user logout and name in NavBar

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
-
+import { logout } from '../actions/userActions'
 
 export default function Header() {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -23,11 +33,20 @@ export default function Header() {
                                 </Nav.Link>
                             </LinkContainer>
 
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <i className='fas fa-user'></i> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title= {userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <i className='fas fa-user'></i> Sign In
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- my-app/src/screens/LoginScreen.js
+++ my-app/src/screens/LoginScreen.js
@@ -18,11 +18,11 @@ const LoginScreen = ({ location, history }) => {
 
   const redirect = location.search ? location.search.split('=')[1] : '/'
 
-  // useEffect(() => {
-  //   if (userInfo) {
-  //     history.push(redirect)
-  //   }
-  // }, [history, userInfo, redirect])
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
 
   const submitHandler = (e) => {
     dispatch(login(email, password))
