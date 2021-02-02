12c0cbc77969600a98dd3e038ce77ab5ff3d0ae6
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-02-02 21:42:28 +0200

    Payment screen

--- my-app/src/screens/ShippingScreen.js
+++ my-app/src/screens/ShippingScreen.js
@@ -30,47 +30,47 @@ const ShippingScreen = ({ history }) => {
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter address'
-                    value={address}
-                    required
-                    onChange={(e) => setAddress(e.target.value)}
-                ></Form.Control>
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter address'
+                        value={address}
+                        required
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId='city'>
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter city'
-                    value={city}
-                    required
-                    onChange={(e) => setCity(e.target.value)}
-                ></Form.Control>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter city'
+                        value={city}
+                        required
+                        onChange={(e) => setCity(e.target.value)}
+                    ></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId='postalCode'>
-                <Form.Label>Postal Code</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter postal code'
-                    value={postalCode}
-                    required
-                    onChange={(e) => setPostalCode(e.target.value)}
-                ></Form.Control>
+                    <Form.Label>Postal Code</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter postal code'
+                        value={postalCode}
+                        required
+                        onChange={(e) => setPostalCode(e.target.value)}
+                    ></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId='country'>
-                <Form.Label>Country</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter country'
-                    value={country}
-                    required
-                    onChange={(e) => setCountry(e.target.value)}
-                ></Form.Control>
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter country'
+                        value={country}
+                        required
+                        onChange={(e) => setCountry(e.target.value)}
+                    ></Form.Control>
                 </Form.Group>
 
                 <Button type='submit' variant='primary'>
