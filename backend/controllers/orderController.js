c981e5c9cd93051a24319a6bdb4e275936d816ae
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-02-07 20:45:20 +0200

    Update order in the back-end

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    Create new order
+// @desc    get order by id
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
@@ -52,3 +52,31 @@ const getOrderById = asyncHandler(async (req, res) => {
 })
 
 export { addOrderItems, getOrderById }
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updateOrder = await order.save()
+
+    res.json(updateOrder)
+    
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
