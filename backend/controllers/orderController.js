4f25d71668d4f46be9ed8d1b6057f5757fb5c7e0
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-02-14 20:59:44 +0200

    Adding PayPal payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,8 +51,6 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems, getOrderById }
-
 // @desc    Update order to paid
 // @route   GET /api/orders/:id/pay
 // @access  Private
