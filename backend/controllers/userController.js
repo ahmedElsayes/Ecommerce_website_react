170dc9c2e35c98373db4dc4c79351ff5dd23422f
Ahmed Elsayes <mysterious.en2010@gmail.com>
2021-02-27 19:22:00 +0200

    Admin can call all users & admin middleware.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,4 +105,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private
+const getUsers = asyncHandler(async (req, res) => {
+    // res.send('successful calling')
+    const users = await User.find({})
+    res.json(users)
+})
+
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
